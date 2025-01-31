4ac9d0e7ee4efab536fe03984fa92d8f0aad4470
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-01-31 16:26:03 +0200

    corrected_views_and_urls_home_to_index

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,10 @@
 from django.contrib.sitemaps.views import index
 from django.urls import path
 
-from . import views
-from .views import home, toggle_task, delete_task, tag_list, add_tag, add_task
+from .views import toggle_task, delete_task, tag_list, add_tag, add_task
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", index, name="index"),
     path("task/<int:task_id>/toggle/", toggle_task, name="toggle_task"),
     path("task/<int:task_id>/delete/", delete_task, name="delete_task"),
     path("tags/", tag_list, name="tag_list"),
